52f3de6d38cd1aea7d6c8cc18a9c50c6144423f0
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2023-05-17 12:22:55 +0300

    create class MyList

--- my_list.rb
+++ my_list.rb
@@ -4,4 +4,13 @@
 require_relative 'my_enumerable'
 
 # getting data from the module
-
+class MyList
+  include MyEnumerable
+  attr_reader :list
+  def initialize(*elements)
+    @list = elements
+  end
+  def each(&block)
+    @list.each(&block)
+  end
+end
