60853e24814242c4e2c7e084b39cdb31d4ea27f2
alexiscyber14 <142sledgehammer14@gmail.com>
2023-05-17 10:45:38 +0100

    add solution

--- my_list.rb
+++ my_list.rb
@@ -7,9 +7,11 @@ require_relative 'my_enumerable'
 class MyList
   include MyEnumerable
   attr_reader :list
+
   def initialize(*elements)
     @list = elements
   end
+
   def each(&block)
     @list.each(&block)
   end
